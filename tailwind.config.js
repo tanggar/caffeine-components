39cb47e3bb3b8290e789106ef0bec5b935307249
Gary Tang <garytang@outlook.com>
2026-09-12 22:25:08 -0400

    Lint tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 
-import { black, blue, green, neutral, white } from "./src/tokens/palette";
-import colors from "./src/tokens/colors";
-import spacing from "./src/tokens/spacing";
-import borderRadius from "./src/tokens/border-radius";
-import { fontFamily, fontWeight } from "./src/tokens/typography";
+import { black, blue, green, neutral, white } from './src/tokens/palette';
+import colors from './src/tokens/colors';
+import spacing from './src/tokens/spacing';
+import borderRadius from './src/tokens/border-radius';
+import { fontFamily, fontWeight } from './src/tokens/typography';
 
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     colors: {
-      transparent: "transparent",
-      current: "currentColor",
+      transparent: 'transparent',
+      current: 'currentColor',
       black,
       blue,
       green,
